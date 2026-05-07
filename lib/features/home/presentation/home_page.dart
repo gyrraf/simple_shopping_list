ffde88373f121b3577feab16f0fc8f2620f028d6
gyrraf <raffael.gyr@hotmail.com>
2026-05-07 22:14:27 +0200

    :globe_with_meridians: start using the internationalization library

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:simple_shopping_list/features/edit/presentation/edit_dialog.dart';
 import 'package:simple_shopping_list/features/home/data/item.dart';
 import 'package:simple_shopping_list/features/home/presentation/home_list.dart';
+import 'package:simple_shopping_list/l10n/app_localizations.dart';
 
 enum _Tab {
   todo(label: "‼️Einkaufen‼️"),
@@ -22,9 +23,7 @@ enum _Tab {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -88,6 +87,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
     final tabValues = _Tab.values;
 
     return DefaultTabController(
@@ -95,7 +95,7 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+          title: Text(l10n.appName),
           bottom: TabBar(
             indicatorSize: TabBarIndicatorSize.tab,
             tabs: tabValues.map((tab) => Tab(text: tab.label)).toList(),
